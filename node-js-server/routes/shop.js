47ffe8545ea97c2d33c1ae677ef058df7484528e
Shadypio <enzuccioamendola@gmail.com>
2024-04-15 17:20:17 +0200

    fix: downloaded new content

--- node-js-server/routes/shop.js
+++ node-js-server/routes/shop.js
@@ -1,11 +1,15 @@
-const path = require("path");
-const express = require("express");
-const router = express.Router();
-const rootDir = require("../util/path");
+const path = require('path');
 
-router.get("/", (req, res, next) => {
-	console.log("In another middleware!");
-	res.sendFile(path.join(rootDir, "views", "shop.html"));
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products);
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
