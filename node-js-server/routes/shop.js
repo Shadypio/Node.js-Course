9cbc832303c27b0421cd8c7c9599a360268d16a0
Shadypio <enzuccioamendola@gmail.com>
2024-04-17 21:21:43 +0200

    feat: ejs template engine

--- node-js-server/routes/shop.js
+++ node-js-server/routes/shop.js
@@ -1,20 +1,22 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-	/*  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html')); */
-	res.render("shop", {
-		prods: adminData.products,
-		pageTitle: "Shop",
-		path: "/",
-	});
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
