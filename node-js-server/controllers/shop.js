92cd9279f3d464c4ba0c677518c1be4a08478ca0
Shadypio <enzuccioamendola@gmail.com>
2024-04-19 14:51:29 +0200

    feat: enhanced app adding new routes

---
+++ node-js-server/controllers/shop.js
@@ -0,0 +1,42 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+	Product.fetchAll(products =>
+		res.render("shop/product-list", {
+			prods: products,
+			pageTitle: "All Products",
+			path: "/products",
+		})
+	);
+};
+
+exports.getIndex = (req, res, next) => {
+	Product.fetchAll(products =>
+		res.render("shop/index", {
+			prods: products,
+			pageTitle: "Shop",
+			path: "/",
+		})
+	);
+};
+
+exports.getCart = (req, res, next) => {
+	res.render("shop/cart", {
+		pageTitle: "Your Cart",
+		path: "/cart",
+	});
+};
+
+exports.getOrders = (req, res, next) => {
+	res.render("shop/orders", {
+		pageTitle: "Your Orders",
+		path: "/orders",
+	});
+};
+
+exports.getCheckout = (req, res, next) => {
+	res.render("shop/checkout", {
+		pageTitle: "Checkout",
+		path: "/checkout",
+	});
+};
